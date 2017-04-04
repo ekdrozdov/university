66d22405a8e748cab1456a6b46d59fc9ffe5c41e
Eduard <ekdrozdov@gmail.com>
2017-04-04 07:22:52 +0700

    Add files via upload
    cp's lab 3 was added

---
+++ cp_l3/l3.for
@@ -0,0 +1,18 @@
+* Laboratory work
+* Variant 13.2
+*
+* This file contain implementation of
+* 1) matrix storage in sparse columnar format
+* 2) multiplication of a matrix by a vector
+
+      dimension mem(1000)
+      open(1, file = 'dimensions.txt')
+      read(1, *)i_a, i_b
+      print *, i_a, i_b
+
+
+1     format(A11)
+      END
+
+
+
---
+++ cp_l3/l3.mk
@@ -0,0 +1,3 @@
+project : C:\Users\ekdro\Documents\cp_proj\l3.exe .SYMBOLIC
+
+!include C:\Users\ekdro\Documents\cp_proj\l3.mk1
