7aaf223ec57cc7aef364ebe34d923d2b9515ecc6
Eduard <ekdrozdov@gmail.com>
2018-04-27 00:08:46 +0700

    some changes in lab_6

--- resources_management/lab_6/main.cpp
+++ resources_management/lab_6/main.cpp
@@ -19,43 +19,40 @@
  */
 
 int main() {
-	int out = dup(1);
-	int s_in = dup(0);
 	printf("Main started\n");
-	int fd;
 
 	printf("Executing 'ls -la'\n");
 	// Execute "ls -la" with an "a.txt" as an output stream.
-	fd = open("a.txt", O_WRONLY | O_CREAT);
-	close(1);
-	fcntl(fd, F_DUPFD, 1);
 	int lsPid = fork();
 	if (lsPid == -1) {
 		perror("fork");
 		_exit(EXIT_FAILURE);
 	}
 	if (lsPid == 0) {
-		execlp("ls", "ls", "-la");
+		int fd;
+		fd = open("a.txt", O_WRONLY | O_CREAT);
+		close(1);
+		fcntl(fd, F_DUPFD, 1);
+		execlp("ls", "ls", "-la", NULL);
 		_exit(EXIT_SUCCESS);
 	}
 
-	dup2(out, 1);
 	printf("Executing 'ps'\n");
 	// Execute "ps" with a "b.txt" as an output stream.
-	fd = open("b.txt", O_WRONLY | O_CREAT);
-	close(1);
-	fcntl(fd, F_DUPFD, 1);
 	int psPid = fork();
 	if (psPid == -1) {
 		perror("fork");
 		_exit(EXIT_FAILURE);
 	}
 	if (psPid == 0) {
-		execlp("ps", "ps");
+		int fd;
+		fd = open("b.txt", O_WRONLY | O_CREAT);
+		close(1);
+		fcntl(fd, F_DUPFD, 1);
+		execlp("ps", "ps", NULL);
 		_exit(EXIT_SUCCESS);
 	}
 
-	dup2(out, 1);
 	printf("Waiting for childs to finish\n");
 	// Waiting for childs to finish.
 	wait(NULL);
@@ -64,49 +61,45 @@ int main() {
 	printf("Executing 'cat'\n");
 	// Execute "cat a.txt b.txt" and pass result to
 	// "sort" via pipe "fd2".
-	int fd2[2];
-	pipe(fd2);
-	close(1);
-	fcntl(fd2[1], F_DUPFD, 1);
+	int fd[2];
+	pipe(fd);
 	int catPid = fork();
 	if (catPid == -1) {
 		perror("fork");
 		_exit(EXIT_FAILURE);
 	}
 	if (catPid == 0) {
-		execlp("cat", "cat", "a.txt");
+		close(1);
+		fcntl(fd[1], F_DUPFD, 1);
+		execlp("cat", "cat", "a.txt", "b.txt", NULL);
 		_exit(EXIT_SUCCESS);
 	}
 
 	//printf("Cat result: %s\n", fd2[1]);
-	dup2(out, 1);
 	printf("Waiting for childs to finish\n");
 	// Wait for a "cat" to finish due to pipe mechanic.
 	wait(NULL);
-
-	char buff[512];
-	read(fd2[0], &buff, sizeof(buff));
-	printf("Pipe content:\n%s\n", buff);
-	write(fd2[1], &buff, sizeof(buff));
-
+	
+	char buff[8192];
+	read(fd[0], &buff, sizeof(buff));
+	printf("\nPipe content:\n%s\n", buff);
+	write(fd[1], &buff, sizeof(buff));
+	
 	printf("Executing sort\n");
 	// Execute "sort" with "cat" output as an input stream
 	// and standart output as an output stream.
-	close(0);
-	fcntl(fd2[0], F_DUPFD, 0);
-	close(1);
-	dup2(out, 1);
 	int sortPid = fork();
 	if (sortPid == -1) {
 		perror("fork");
 		_exit(EXIT_FAILURE);
 	}
 	if (sortPid == 0) {
-		execlp("sort", "sort", 0);
+		close(0);
+		fcntl(fd[0], F_DUPFD, 0);
+		execlp("sort", "sort", NULL);
 		_exit(EXIT_SUCCESS);
 	}
 
-	dup2(out, 1);
 	printf("Main finished\n");
 
 	return 0;
