2773b9db84f55e3036d58bbf2760e68426c3c858
Eduard <ekdrozdov@gmail.com>
2018-06-25 17:24:29 +0700

    lab 8 complete

--- resources_management/lab_8/client.cpp
+++ resources_management/lab_8/client.cpp
@@ -1,47 +1,36 @@
-#pragma comment (lib,"Ws2_32.lib")
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <cstdlib>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#define PORT_NUM 2088
+#include <stdio.h>
 
-int main()
-{
-	int sock;
-	int err;
-	struct sockaddr_in addr;
-	char ip[30];
-	char recvbuf[256];
-	
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(PORT_NUM);
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	
-	printf("Client is created\n");
-	err = connect(sock, (struct sockaddr*)&addr, sizeof(addr));
-	
-	if (err == -1)
-	{
-		printf("Can't connect to server\n");
-		return 1;
+int main(int argc, char** argv) {
+	// Create socket.
+	int serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (serv_sock < 0) {
+		perror("bind failed");
+		return EXIT_FAILURE;
 	}
-	printf("List of users IP:\n");
-	recv(sock, recvbuf, sizeof(recvbuf), 0);
-	printf("%s\n", recvbuf);
-	while (strcmp(recvbuf, "that's all") != 0)
-	{
-		recv(sock, recvbuf, sizeof(recvbuf), 0);
-		printf("%s\n", recvbuf);
+	printf("Socket created\n");
+
+	// Connection request.
+	struct sockaddr_in serv_addr;
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(8000);
+	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	printf("Connection request\n");
+	if (connect(serv_sock, (struct sockaddr *) &serv_addr,
+				sizeof(serv_addr)) < 0) {
+		perror("connection failed");
+		return EXIT_FAILURE;
 	}
-	while (1)
-	{
-		recv(sock, recvbuf, sizeof(recvbuf), 0);
-		printf("The new user's IP is: %s\n", recvbuf);
-	}
+
+	const char* text = "Hello";
+	printf("Send text '%s'\n", text);
+	send(serv_sock, text, sizeof(text), 0);
+
+	printf("Press any key to exit...\n");
+	getchar();
+	return EXIT_SUCCESS;
 }
--- resources_management/lab_8/server.cpp
+++ resources_management/lab_8/server.cpp
@@ -1,75 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <cstdlib>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#define PORT_NUM 2088
-int Sockets[15];
-struct sockaddr_in AllAddr[15]; 
+#include <stdio.h>
 
-int main()
-{
-	int ClientSock;
-	int i;
-	int err;
-	int ListSock;
-	int ConnectSock;
-	struct sockaddr_in addr;
-	int curr = 0;
-	
-	ListSock = socket(AF_INET, SOCK_STREAM, 0);
-	
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(PORT_NUM);
-	addr.sin_addr.s_addr = INADDR_ANY;
-	
-	err = bind(ListSock, (struct sockaddr *) &addr, sizeof(addr));
-	if (err == -1)
-	{
-		printf("Can't bind");
-		return 1;
+int main(int argc, char** argv) {
+	// Create socket.
+	int serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (serv_sock < 0) {
+		perror("bind failed");
+		return EXIT_FAILURE;
 	}
-	printf("Binded\n");
-	err = listen(ListSock, 10);
-	if (err == -1)
-	{
-		printf("Can't listen");
-			return 1;
+	printf("Socket created\n");
+
+	// Bind socket.
+	struct sockaddr_in serv_addr;
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(8000);
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if (bind(serv_sock, (struct sockaddr *) &serv_addr,
+				sizeof(serv_addr)) < 0) {
+		perror("bind failed");
+		return EXIT_FAILURE;
 	}
-	printf("Listening\n");
-	while (1)
-	{
-		struct sockaddr_in from;
-		int fromlen = sizeof(from);
-		ClientSock = accept(ListSock, (struct sockaddr*)&from, &fromlen);
-		if (ClientSock == -1)
-		{
-			printf("Can't accept\n");
-			return 1;
-		}
-		printf("Somebody is accepted\n");
-		printf("Sending data to clients\n");
-		if (curr < 15)
-		{
-			AllAddr[curr] = from;
-			Sockets[curr] = ClientSock;
-			for (i = 0; i < curr; i++)
-			{	
-				send(Sockets[i], inet_ntoa(from.sin_addr), 256, 0);
-				send(ClientSock, inet_ntoa(AllAddr[i].sin_addr), 256, 0);
-			}
-			send(ClientSock, "that's all", 256, 0);
-			curr++;
-		}
-		else 
-		{
-			printf("Too much people on the lane");
-			return 1;
-		}
-		printf("Data is sended\n");
-		ClientSock = -1;
+	printf("Socket binded\n");
+
+	// Listen socket.
+	int queue_size = 1;
+	printf("Listening socket...\n");
+	if (listen(serv_sock, queue_size) < 0) {
+		perror("listen failed");
+		return EXIT_FAILURE;
 	}
+	
+	// Accept connection.
+	int client_sock = accept(serv_sock, NULL, NULL);
+	if (client_sock < 0) {
+		perror("accept failed");
+		return EXIT_FAILURE;
+	}
+	printf("Connection accepted\n");
+
+	char buf[128] = { '\0' };
+	recv(client_sock, buf, sizeof(buf), 0);
+	printf("Recived text: '%s'\n", buf);
+
+	printf("Press any key to exit...\n");
+	getchar();
+	return EXIT_SUCCESS;
 }
